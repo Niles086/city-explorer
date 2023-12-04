482cfb82e2212e2dd3eb9ec62e0220edd208b323
Niles Thompson <dewand.th@gmail.com>
2023-12-04 14:09:31 -0800

    added map feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,11 @@ export default function App() {
   const [searchQuery, setSearchQuery] = useState('');
 
   async function getLocation() {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`
+    let API = `https://us1.locationiq.com/v1/search.php?key=${API_KEY}&q=${searchQuery}&format=json`;
+   
     const response = await axios.get(API);
     const data = response.data[0];
-    console.log(data);
+   
     setLocation(data);
   }
 
@@ -22,18 +23,27 @@ export default function App() {
   function onSearchChange(event) {
     setSearchQuery(event.target.value);
   }
-function searchLocation(event) {
-  event.preventDefault();
+
+  function searchLocation(event) {
+    event.preventDefault();
     getLocation();
-  setSearchQuery({display_name: searchQuery});
+    setSearchQuery({ display_name: searchQuery });
+  }
+function generateMap(lat, lon) {
+  const API2 = `https://maps.locationiq.com/v3/staticmap?key=${API_KEY}&center=${lat},${lon}&zoom=10`;
+  return API2;
 }
   return (
     <>
-    <input onChange={onSearchChange} />
-    <button onClick={searchLocation}>Search</button>
+      <input onChange={onSearchChange} />
+      <button onClick={searchLocation}>Search</button>
       <h2>The city is:{location.display_name}</h2>
       <h2>Latitude:{location.lat}</h2> <h2>Longitude:{location.lon}</h2>
-      
+      {location.lat && location.lon && <img src={generateMap(location.lat, location.lon)} 
+      alt="map"
+      style={{maxWidth: '80%'}}
+      />}
+
     </>
   )
 }
