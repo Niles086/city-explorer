d5b94daf7ac0d59b6f6c78db124bb1583270bf6e
Niles Thompson <dewand.th@gmail.com>
2023-12-10 18:53:47 -0800

    refactored code

--- src/components/weather.jsx
+++ src/components/weather.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import { Accordion } from 'react-bootstrap';
 
+// Nested WeatherDay component
+const WeatherDay = ({ date, description }) => (
+  <div>
+    <p>Date: {date}</p>
+    <p>Description: {description}</p>
+    {/* Additional content specific to a single day can be added here */}
+  </div>
+);
+
+// RenderWeather component
 export default function RenderWeather(props) {
   return (
     <Accordion defaultActiveKey="0">
@@ -8,8 +18,7 @@ export default function RenderWeather(props) {
         <Accordion.Item key={index} eventKey={index.toString()}>
           <Accordion.Header>{item.date}</Accordion.Header>
           <Accordion.Body>
-            <p>{item.description}</p>
-            {/* Additional content can be added here */}
+            <WeatherDay date={item.date} description={item.description} />
           </Accordion.Body>
         </Accordion.Item>
       ))}
